cfef855ec616724d12ce2176ea1927450ccc3eed
diedline <1019348254@qq.com>
2019-09-02 18:54:02 +0800

    数据清洗

--- src/main/java/com/diedline/ETL/ETLDriver.java
+++ src/main/java/com/diedline/ETL/ETLDriver.java
@@ -1,14 +1,22 @@
 package com.diedline.ETL;
 
 import org.apache.hadoop.conf.Configuration;
+import org.apache.hadoop.fs.Path;
 import org.apache.hadoop.io.NullWritable;
 import org.apache.hadoop.io.Text;
 import org.apache.hadoop.mapreduce.Job;
+import org.apache.hadoop.mapreduce.lib.input.FileInputFormat;
+import org.apache.hadoop.mapreduce.lib.output.FileOutputFormat;
 import org.apache.hadoop.util.Tool;
+import org.apache.hadoop.util.ToolRunner;
 
 public class ETLDriver implements Tool {
     private Configuration conf;
 
+    public static void main(String[] args) throws Exception {
+        int run = ToolRunner.run(new ETLDriver(), args);
+        System.out.println(run);
+    }
     public int run(String[] strings) throws Exception {
         //1.获取Job对象
         Job job = Job.getInstance(getConf());
@@ -20,11 +28,14 @@ public class ETLDriver implements Tool {
         job.setMapOutputKeyClass(Text.class);
         job.setMapOutputValueClass(NullWritable.class);
         //5.最终输出类型
-
+        job.setOutputKeyClass(Text.class);
+        job.setMapOutputValueClass(NullWritable.class);
         //6.输入输出路径
-
+        FileInputFormat.setInputPaths(job, new Path(strings[0]));
+        FileOutputFormat.setOutputPath(job, new Path(strings[1]));
         //7.提交任务
-        return 0;
+        boolean result = job.waitForCompletion(true);
+        return result ? 0 : 1;
     }
 
     public void setConf(Configuration configuration) {
